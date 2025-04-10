0d7361ceebc4458d733ad30558842436846fe575
Md Al Hossain Mukib <143283871+mdmokib@users.noreply.github.com>
2025-04-10 13:44:57 +0600

    Add files via upload

---
+++ debugAFizzBuzz.py
@@ -0,0 +1,26 @@
+# Which year do you want to check?
+"""year = int(input())
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    
+    print("Leap year.")
+else:
+  print("Not leap year.")"""
+
+
+target = int(input())
+for number in range(1, target + 1):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
